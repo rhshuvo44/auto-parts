ab760bb3d26dbec981b7422bf05ccb45759170d9
Ripon Hossain Shuvo <rhshuvo44@gmail.com>
2022-05-25 00:02:38 +0600

    purchase

--- src/pages/Purchase/Purchase.js
+++ src/pages/Purchase/Purchase.js
@@ -75,6 +75,17 @@ const Purchase = () => {
                 </div>
                 <div className="form-control w-full max-w-xs">
                   <label className="label">
+                    <span className="label-text">Product Name</span>
+                  </label>
+                  <input
+                    type="text"
+                    placeholder="Product Name"
+                    className="input input-bordered w-full max-w-xs"
+                    {...register("productName")}
+                  />
+                </div>
+                <div className="form-control w-full max-w-xs">
+                  <label className="label">
                     <span className="label-text">Phone</span>
                   </label>
                   <input
