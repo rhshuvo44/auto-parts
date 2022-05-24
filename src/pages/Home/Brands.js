fd282166c0979197c39209b57f01a985ec58ec92
Ripon Hossain Shuvo <rhshuvo44@gmail.com>
2022-05-24 10:45:42 +0600

    business

---
+++ src/pages/Home/Brands.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import { Pagination } from "swiper";
+import 'swiper/css';
+import "swiper/css/pagination";
+import { Swiper, SwiperSlide } from 'swiper/react';
+
+
+
+const Brands = () => {
+    return (
+        <>
+        <Swiper
+          slidesPerView={3}
+          spaceBetween={30}
+          pagination={{
+            clickable: true,
+          }}
+          modules={[Pagination]}
+          className="mySwiper"
+        >
+          <SwiperSlide>Slide 1</SwiperSlide>
+          <SwiperSlide>Slide 2</SwiperSlide>
+          <SwiperSlide>Slide 3</SwiperSlide>
+          <SwiperSlide>Slide 4</SwiperSlide>
+          <SwiperSlide>Slide 5</SwiperSlide>
+          <SwiperSlide>Slide 6</SwiperSlide>
+          <SwiperSlide>Slide 7</SwiperSlide>
+          <SwiperSlide>Slide 8</SwiperSlide>
+          <SwiperSlide>Slide 9</SwiperSlide>
+        </Swiper>
+      </>
+    );
+};
+
+export default Brands;
--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Banner from './Banner';
+import Brands from './Brands';
 import BusinessSummary from './BusinessSummary';
 
 const Home = () => {
@@ -7,6 +8,7 @@ const Home = () => {
         <div>
             <Banner/>
             <BusinessSummary/>
+            <Brands/>
         </div>
     );
 };
